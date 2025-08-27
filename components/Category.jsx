795a60b9476f5afb5d4cfa32d6f9a934aee56f89
Onimole Tosin <onimolesodiq282@gmail.com>
2025-08-27 02:01:50 +0100

    Category mappping

--- components/Category.jsx
+++ components/Category.jsx
@@ -1,6 +1,7 @@
+import Image from "next/image";
 import React, { useEffect, useState } from "react";
+import ProductCard from "./ProductCard";
 
-// const categories = ["Beauty", "Perfume", "Fragrancess", "Skincare", "Makeup"]
 
 const fetchCategories = async (callback) => {
     const res = await fetch('https://dummyjson.com/products/category-list')
@@ -8,31 +9,64 @@ const fetchCategories = async (callback) => {
     callback(data)
 }
 
+const fetchProduct = async (cat, callback) => {
+    const res = await fetch(`https://dummyjson.com/products/category/${cat}`)
+    const data = await res.json()
+    callback(data.products)
+}
+
 
 
 const Category = () => {
     const [categories, setCategories] = useState([])
+    const [ifSelected, setIfSelected] = useState()
+    const [products, setProduct] = useState([])
 
 
-    
     useEffect(() => {
-        fetchCategories(setCategories)
+        fetchCategories(data => setCategories(data))
+
     }, [])
 
-    return (
-        <div className="p-4 ">
-            <h2 className="text-xl font-bold mb-3">Categories</h2>
+    useEffect(() => {
+        fetchProduct(ifSelected, setProduct)
 
-            <ul className="space-y-2 flex flex-row gap-7">
-                {categories.map(cat => (
-                    <li key={cat}
-                        className="cursor-pointer p-2 rounded-lg bg-gray-100 hover:bg-gray-200">
-                        {cat}
-                    </li>
-                ))
-                }
-            </ul>
-        </div>
+    }, [ifSelected])
+
+    return (
+        <>
+            <div className="relative w-full">
+                <h2 className="text-xl font-bold mb-3">Categories</h2>
+
+                <ul className="space-y-2 overflow-x-auto flex flex-row gap-7">
+                    {categories.map(cat => (
+                        <button
+                            key={cat}
+                            onClick={() => setIfSelected(cat)}
+                            className="cursor-pointer h-fit p-2 min-w-[150px] flex rounded-lg bg-gray-100 hover:bg-gray-200"
+                        >
+                            <p className="">
+                                {cat}
+                            </p>
+                        </button>
+                    ))
+                    }
+                </ul>
+            </div>
+
+            {ifSelected && (
+                <div>
+                    <h2 className="text-xl font-bold mb-3">{ifSelected}</h2>
+                    <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 flex-col items-center gap-6 mt-6 pb-14 w-full">
+                        {
+                            products.map((product, index) => <ProductCard key={index} product={product} />
+                            )}
+                    </div>
+
+                </div>
+            )}
+
+        </>
     );
 };
 
